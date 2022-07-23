e8935cf1660a3dc3e4100017d8453d4a2f95f4f5
Masoumi-shima <masoumi.shimaa@gmail.com>
2022-07-23 19:25:39 -0400

    Refactoring

--- src/main/java/com/todo/TodoWebApp/todoList/TodoList.java
+++ src/main/java/com/todo/TodoWebApp/todoList/TodoList.java
@@ -1,15 +1,11 @@
 package com.todo.TodoWebApp.todoList;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.time.Instant;
 
 @Entity
